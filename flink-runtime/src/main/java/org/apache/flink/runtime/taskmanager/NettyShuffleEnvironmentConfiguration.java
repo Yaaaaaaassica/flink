d81fe47f25ff5fd9b46db76f6856c801d5977f7d
spafka <spafka@163.com>
2019-07-24 22:48:09 +0800

    使用remotechannel

--- flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/NettyShuffleEnvironmentConfiguration.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/NettyShuffleEnvironmentConfiguration.java
@@ -477,7 +477,7 @@ public class NettyShuffleEnvironmentConfiguration {
 		int dataport) {
 
 		final NettyConfig nettyConfig;
-		if (!localTaskManagerCommunication) {
+	//	if (!localTaskManagerCommunication) {
 			final InetSocketAddress taskManagerInetSocketAddress = new InetSocketAddress(taskManagerAddress, dataport);
 
 			nettyConfig = new NettyConfig(
@@ -486,9 +486,9 @@ public class NettyShuffleEnvironmentConfiguration {
 				ConfigurationParserUtils.getPageSize(configuration),
 				ConfigurationParserUtils.getSlot(configuration),
 				configuration);
-		} else {
-			nettyConfig = null;
-		}
+//		} else {
+//			nettyConfig = null;
+//		}
 
 		return nettyConfig;
 	}
