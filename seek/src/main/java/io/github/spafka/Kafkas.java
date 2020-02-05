59255702185c7286d4ce07e464b5c0ed898e2de9
spafka <spafka@163.com>
2020-02-06 02:23:59 +0800

    add kafka server

---
+++ seek/src/main/java/io/github/spafka/Kafkas.java
@@ -0,0 +1,70 @@
+package io.github.spafka;
+
+
+import kafka.server.KafkaServerStartable;
+import org.apache.commons.io.FileUtils;
+import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class Kafkas {
+
+	static final String TOPIC = "test";
+	static final String CG = "spafka";
+
+	public static void main(String[] args) throws InterruptedException {
+
+
+		new Thread(() -> {
+			try {
+				FileUtils.deleteDirectory(new File("/tmp/zookeeper"));
+
+				QuorumPeerConfig config = new QuorumPeerConfig();
+				InputStream is = Kafkas.class.getResourceAsStream("/zookeeper.properties");
+				Properties p = new Properties();
+				p.load(is);
+				config.parseProperties(p);
+				ServerConfig serverconfig = new ServerConfig();
+				serverconfig.readFrom(config);
+				new ZooKeeperServerMain().runFromConfig(serverconfig);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (QuorumPeerConfig.ConfigException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		TimeUnit.SECONDS.sleep(5);
+
+		IntStream.rangeClosed(1, 3).forEach(x -> {
+			new Thread(() -> {
+				try {
+					FileUtils.deleteDirectory(new File("/tmp/kafka/data" + x));
+
+
+					InputStream is = Kafkas.class.getResourceAsStream("/server" + x + ".properties");
+					Properties p = new Properties();
+					p.load(is);
+					is.close();
+					KafkaServerStartable kafkaServerStartable = KafkaServerStartable.fromProps(p);
+					kafkaServerStartable.startup();
+					kafkaServerStartable.awaitShutdown();
+				} catch (IOException e) {
+
+				}
+
+
+			}).start();
+		});
+
+
+	}
+
+}
